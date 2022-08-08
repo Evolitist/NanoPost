7656fee553d2edcb08f3fb07b1499805d72f7302
Evolitist <evolitist@gmail.com>
2022-08-08 22:03:30 +0700

    add convenience extensions for PaddingValues

---
+++ app/src/main/java/com/evolitist/nanopost/presentation/extensions/PaddingValuesExt.kt
@@ -0,0 +1,40 @@
+package com.evolitist.nanopost.presentation.extensions
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+
+fun PaddingValues.copy(
+    layoutDirection: LayoutDirection,
+    start: Dp = calculateStartPadding(layoutDirection),
+    top: Dp = calculateTopPadding(),
+    end: Dp = calculateEndPadding(layoutDirection),
+    bottom: Dp = calculateBottomPadding(),
+) = PaddingValues(start, top, end, bottom)
+
+@Composable
+fun PaddingValues.copy(
+    start: Dp = calculateStartPadding(LocalLayoutDirection.current),
+    top: Dp = calculateTopPadding(),
+    end: Dp = calculateEndPadding(LocalLayoutDirection.current),
+    bottom: Dp = calculateBottomPadding(),
+) = PaddingValues(start, top, end, bottom)
+
+@Composable
+operator fun PaddingValues.plus(
+    other: PaddingValues,
+): PaddingValues {
+
+    val ld = LocalLayoutDirection.current
+
+    return PaddingValues(
+        start = calculateStartPadding(ld) + other.calculateStartPadding(ld),
+        top = calculateTopPadding() + other.calculateTopPadding(),
+        end = calculateEndPadding(ld) + other.calculateEndPadding(ld),
+        bottom = calculateBottomPadding() + other.calculateBottomPadding(),
+    )
+}
