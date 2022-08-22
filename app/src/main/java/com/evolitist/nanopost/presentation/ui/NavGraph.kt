00d441631d21500e2acb60ede5022e660ffbd570
Evolitist <evolitist@gmail.com>
2022-08-22 18:27:44 +0700

    extract navigation to type-safe stable class

--- app/src/main/java/com/evolitist/nanopost/presentation/ui/NavGraph.kt
+++ app/src/main/java/com/evolitist/nanopost/presentation/ui/NavGraph.kt
@@ -12,14 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.dialog
 import androidx.navigation.navArgument
+import com.evolitist.nanopost.presentation.ui.NanoPostDestinationArgs.IdArg
 import com.evolitist.nanopost.presentation.ui.auth.AuthScreen
 import com.evolitist.nanopost.presentation.ui.create.post.CreatePostScreen
 import com.evolitist.nanopost.presentation.ui.create.profile.CreateProfileScreen
@@ -31,7 +32,6 @@ import com.evolitist.nanopost.presentation.ui.posts.PostsScreen
 import com.evolitist.nanopost.presentation.ui.profile.ProfileScreen
 import com.evolitist.nanopost.presentation.ui.subscribers.SubscribersScreen
 import com.evolitist.nanopost.presentation.ui.view.AlertDialogContent
-import com.evolitist.nanopost.presentation.ui.view.ModalBottomSheetLayout
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -41,296 +41,195 @@ import soup.compose.material.motion.animation.materialSharedAxisXIn
 import soup.compose.material.motion.animation.materialSharedAxisXOut
 import soup.compose.material.motion.animation.rememberSlideDistance
 
-enum class Screen(
-    val route: String,
-) {
-    NavGraph("nav_graph"),
-    Root("root"),
-    Auth("auth"),
-    Logout("logout"),
-    Feed("feed"),
-    Search("search"),
-    Profile("profile"),
-    Subscribers("subscribers"),
-    Images("images"),
-    Posts("posts"),
-    CreatePost("create_post"),
-    CreateProfile("create_profile"),
-    Post("post"),
-    Image("image");
-}
-
 @OptIn(ExperimentalMaterialNavigationApi::class)
 @Composable
 fun AppNavGraph() {
     val activityViewModel = viewModel<MainViewModel>()
     val bottomSheetNavigator = rememberBottomSheetNavigator()
     val navController = rememberAnimatedNavController(bottomSheetNavigator)
+    val navigationActions = remember(navController) {
+        NanoPostNavigationActions(navController)
+    }
 
     val showAuth by activityViewModel.showAuthFlow.collectAsState()
 
     LaunchedEffect(showAuth) {
         when (showAuth) {
-            true -> navController.navigate(Screen.Auth.route) {
-                popUpTo(Screen.NavGraph.route)
-            }
-            false -> navController.navigate(Screen.Feed.route) {
-                popUpTo(Screen.NavGraph.route)
-            }
+            true -> navigationActions.switchToAuth()
+            false -> navigationActions.switchToFeed()
             else -> Unit
         }
     }
 
-    ModalBottomSheetLayout(
-        bottomSheetNavigator = bottomSheetNavigator,
-    ) {
-        Scaffold { innerPadding ->
-            val slideDistance = rememberSlideDistance()
-            AnimatedNavHost(
-                navController = navController,
-                route = Screen.NavGraph.route,
-                startDestination = Screen.Root.route,
-                enterTransition = { materialSharedAxisXIn(true, slideDistance) },
-                exitTransition = { materialSharedAxisXOut(true, slideDistance) },
-                popEnterTransition = { materialSharedAxisXIn(false, slideDistance) },
-                popExitTransition = { materialSharedAxisXOut(false, slideDistance) },
-                modifier = Modifier.padding(innerPadding),
-            ) {
-                composable(
-                    route = Screen.Root.route,
-                ) {
-                    Surface(modifier = Modifier.fillMaxSize()) {
-                        Box(
-                            contentAlignment = Alignment.Center,
-                        ) {
-                            CircularProgressIndicator()
-                        }
+    Scaffold { innerPadding ->
+        val slideDistance = rememberSlideDistance()
+        AnimatedNavHost(
+            navController = navController,
+            startDestination = NanoPostDestinations.RootRoute,
+            enterTransition = { materialSharedAxisXIn(true, slideDistance) },
+            exitTransition = { materialSharedAxisXOut(true, slideDistance) },
+            popEnterTransition = { materialSharedAxisXIn(false, slideDistance) },
+            popExitTransition = { materialSharedAxisXOut(false, slideDistance) },
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            composable(NanoPostDestinations.RootRoute) {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        CircularProgressIndicator()
                     }
                 }
-
-                composable(
-                    route = Screen.Auth.route,
-                ) {
-                    AuthScreen()
-                }
-
-                dialog(
-                    route = Screen.Logout.route,
-                ) {
-                    AlertDialogContent(
-                        title = { Text("Logout") },
-                        text = { Text("Are you sure you want to logout?") },
-                        confirmButton = {
-                            TextButton(
-                                onClick = { activityViewModel.logout() },
-                                content = { Text("Confirm") },
-                            )
-                        },
-                        dismissButton = {
-                            TextButton(
-                                onClick = { navController.navigateUp() },
-                                content = { Text("Cancel") },
-                            )
-                        },
-                    )
-                }
-
-                authorizedRoutes(navController)
             }
+
+            composable(NanoPostDestinations.AuthRoute) {
+                AuthScreen()
+            }
+
+            dialog(NanoPostDestinations.LogoutRoute) {
+                AlertDialogContent(
+                    title = { Text("Logout") },
+                    text = { Text("Are you sure you want to logout?") },
+                    confirmButton = {
+                        TextButton(
+                            onClick = activityViewModel::logout,
+                            content = { Text("Confirm") },
+                        )
+                    },
+                    dismissButton = {
+                        TextButton(
+                            onClick = navController::navigateUp,
+                            content = { Text("Cancel") },
+                        )
+                    },
+                )
+            }
+
+            authorizedRoutes(navigationActions)
         }
     }
 }
 
-fun NavGraphBuilder.authorizedRoutes(navController: NavController) {
-    composable(Screen.Feed.route) {
+fun NavGraphBuilder.authorizedRoutes(navigationActions: NanoPostNavigationActions) {
+    composable(NanoPostDestinations.FeedRoute) {
         FeedScreen(
-            onCreatePostClick = {
-                navController.navigate(Screen.CreatePost.route)
-            },
-            onCreateProfileClick = {
-                navController.navigate(Screen.CreateProfile.route)
-            },
-            onPostClick = {
-                navController.navigate("${Screen.Post.route}/$it")
-            },
-            onImageClick = {
-                navController.navigate("${Screen.Image.route}/$it")
-            },
-            onProfileClick = { profileId ->
-                navController.navigate(
-                    Screen.Profile.route + profileId?.let { "?id=$it" }.orEmpty()
-                )
-            },
+            onCreatePostClick = navigationActions::navigateToCreatePost,
+            onCreateProfileClick = navigationActions::navigateToCreateProfile,
+            onPostClick = navigationActions::navigateToPost,
+            onImageClick = navigationActions::navigateToImage,
+            onProfileClick = navigationActions::navigateToProfile,
         )
     }
 
     composable(
-        route = "${Screen.Profile.route}?id={id}",
+        route = NanoPostDestinations.ProfileRoute,
         arguments = listOf(
-            navArgument("id") {
+            navArgument(IdArg) {
                 type = NavType.StringType
                 nullable = true
             }
         ),
     ) { backStackEntry ->
         ProfileScreen(
-            profileId = backStackEntry.arguments?.getString("id"),
-            onCloseClick = {
-                navController.navigateUp()
-            },
-            onLogoutClick = {
-                navController.navigate(Screen.Logout.route)
-            },
-            onSubscribersClick = { profileId ->
-                navController.navigate(
-                    Screen.Subscribers.route + profileId?.let { "?id=$it" }.orEmpty()
-                )
-            },
-            onImagesClick = { profileId ->
-                navController.navigate(
-                    Screen.Images.route + profileId?.let { "?id=$it" }.orEmpty()
-                )
-            },
-            onPostsClick = { profileId ->
-                navController.navigate(
-                    Screen.Posts.route + profileId?.let { "?id=$it" }.orEmpty()
-                )
-            },
-            onCreatePostClick = {
-                navController.navigate(Screen.CreatePost.route)
-            },
-            onCreateProfileClick = {
-                navController.navigate(Screen.CreateProfile.route)
-            },
-            onPostClick = {
-                navController.navigate("${Screen.Post.route}/$it")
-            },
-            onImageClick = {
-                navController.navigate("${Screen.Image.route}/$it")
-            },
-            onProfileClick = {
-                navController.navigate("${Screen.Profile.route}?id=$it")
-            },
+            profileId = backStackEntry.arguments?.getString(IdArg),
+            onCloseClick = navigationActions::navigateUp,
+            onLogoutClick = navigationActions::openLogoutDialog,
+            onSubscribersClick = navigationActions::navigateToSubscribers,
+            onImagesClick = navigationActions::navigateToImages,
+            onPostsClick = navigationActions::navigateToPosts,
+            onCreatePostClick = navigationActions::navigateToCreatePost,
+            onCreateProfileClick = navigationActions::navigateToCreateProfile,
+            onPostClick = navigationActions::navigateToPost,
+            onImageClick = navigationActions::navigateToImage,
+            onProfileClick = navigationActions::navigateToProfile,
         )
     }
 
     composable(
-        route = "${Screen.Subscribers.route}?id={id}",
+        route = NanoPostDestinations.SubscribersRoute,
         arguments = listOf(
-            navArgument("id") {
+            navArgument(IdArg) {
                 type = NavType.StringType
                 nullable = true
             }
         ),
     ) { backStackEntry ->
         SubscribersScreen(
-            profileId = backStackEntry.arguments?.getString("id"),
-            onCloseClick = {
-                navController.navigateUp()
-            },
-            onProfileClick = {
-                navController.navigate("${Screen.Profile.route}?id=$it")
-            },
+            profileId = backStackEntry.arguments?.getString(IdArg),
+            onCloseClick = navigationActions::navigateUp,
+            onProfileClick = navigationActions::navigateToProfile,
         )
     }
 
     composable(
-        route = "${Screen.Images.route}?id={id}",
+        route = NanoPostDestinations.ImagesRoute,
         arguments = listOf(
-            navArgument("id") {
+            navArgument(IdArg) {
                 type = NavType.StringType
                 nullable = true
             }
         ),
     ) { backStackEntry ->
         ImagesScreen(
-            profileId = backStackEntry.arguments?.getString("id"),
-            onCloseClick = {
-                navController.navigateUp()
-            },
-            onImageClick = {
-                navController.navigate("${Screen.Image.route}/$it")
-            },
+            profileId = backStackEntry.arguments?.getString(IdArg),
+            onCloseClick = navigationActions::navigateUp,
+            onImageClick = navigationActions::navigateToImage,
         )
     }
 
     composable(
-        route = "${Screen.Posts.route}?id={id}",
+        route = NanoPostDestinations.PostsRoute,
         arguments = listOf(
-            navArgument("id") {
+            navArgument(IdArg) {
                 type = NavType.StringType
                 nullable = true
             }
         ),
     ) { backStackEntry ->
         PostsScreen(
-            profileId = backStackEntry.arguments?.getString("id"),
-            onCloseClick = {
-                navController.navigateUp()
-            },
-            onPostClick = {
-                navController.navigate("${Screen.Post.route}/$it")
-            },
-            onImageClick = {
-                navController.navigate("${Screen.Image.route}/$it")
-            },
-            onProfileClick = {
-                navController.navigate("${Screen.Profile.route}?id=$it")
-            },
+            profileId = backStackEntry.arguments?.getString(IdArg),
+            onCloseClick = navigationActions::navigateUp,
+            onPostClick = navigationActions::navigateToPost,
+            onImageClick = navigationActions::navigateToImage,
+            onProfileClick = navigationActions::navigateToProfile,
         )
     }
 
     composable(
-        route = Screen.CreatePost.route,
-    ) {
-        CreatePostScreen(
-            onCloseClick = {
-                navController.navigateUp()
-            },
-        )
-    }
-
-    composable(
-        route = Screen.CreateProfile.route,
-    ) {
-        CreateProfileScreen(
-            onCloseClick = {
-                navController.navigateUp()
-            },
-        )
-    }
-
-    composable(
-        route = "${Screen.Post.route}/{id}",
+        route = NanoPostDestinations.ImageRoute,
         arguments = listOf(
-            navArgument("id") { type = NavType.StringType }
-        ),
-    ) { backStackEntry ->
-        PostScreen(
-            postId = backStackEntry.arguments!!.getString("id")!!,
-            onCloseClick = {
-                navController.navigateUp()
-            },
-            onImageClick = {
-                navController.navigate("${Screen.Image.route}/$it")
-            },
-            onProfileClick = {
-                navController.navigate("${Screen.Profile.route}?id=$it")
-            },
-        )
-    }
-
-    composable(
-        route = "${Screen.Image.route}/{id}",
-        arguments = listOf(
-            navArgument("id") { type = NavType.StringType }
+            navArgument(IdArg) { type = NavType.StringType }
         ),
     ) { backStackEntry ->
         ImageScreen(
-            imageId = backStackEntry.arguments!!.getString("id")!!,
-            onCloseClick = {
-                navController.navigateUp()
-            },
+            imageId = backStackEntry.arguments!!.getString(IdArg)!!,
+            onCloseClick = navigationActions::navigateUp,
+        )
+    }
+
+    composable(
+        route = NanoPostDestinations.PostRoute,
+        arguments = listOf(
+            navArgument(IdArg) { type = NavType.StringType }
+        ),
+    ) { backStackEntry ->
+        PostScreen(
+            postId = backStackEntry.arguments!!.getString(IdArg)!!,
+            onCloseClick = navigationActions::navigateUp,
+            onImageClick = navigationActions::navigateToImage,
+            onProfileClick = navigationActions::navigateToProfile,
+        )
+    }
+
+    composable(NanoPostDestinations.CreatePostRoute) {
+        CreatePostScreen(
+            onCloseClick = navigationActions::navigateUp,
+        )
+    }
+
+    composable(NanoPostDestinations.CreateProfileRoute) {
+        CreateProfileScreen(
+            onCloseClick = navigationActions::navigateUp,
         )
     }
 }
--- app/src/main/java/com/evolitist/nanopost/presentation/ui/Navigation.kt
+++ app/src/main/java/com/evolitist/nanopost/presentation/ui/Navigation.kt
@@ -1,29 +1,101 @@
 package com.evolitist.nanopost.presentation.ui
 
+import androidx.compose.runtime.Stable
+import androidx.navigation.NavHostController
+import com.evolitist.nanopost.presentation.ui.NanoPostDestinationArgs.IdArg
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.AuthScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.CreatePostScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.CreateProfileScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.FeedScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.ImageScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.ImagesScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.LogoutScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.PostScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.PostsScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.ProfileScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.RootScreen
+import com.evolitist.nanopost.presentation.ui.NanoPostScreens.SubscribersScreen
+
 private object NanoPostScreens {
-    const val Auth = "auth"
-    const val Feed = "feed"
-    const val Search = "search"
-    const val Profile = "profile"
-    const val CreatePost = "create_post"
-    const val CreateProfile = "create_profile"
-    const val Post = "post"
-    const val Image = "image"
+    const val RootScreen = "root"
+    const val AuthScreen = "auth"
+    const val LogoutScreen = "logout"
+    const val FeedScreen = "feed"
+    const val ProfileScreen = "profile"
+    const val SubscribersScreen = "subscribers"
+    const val ImagesScreen = "images"
+    const val PostsScreen = "posts"
+    const val ImageScreen = "image"
+    const val PostScreen = "post"
+    const val CreatePostScreen = "create_post"
+    const val CreateProfileScreen = "create_profile"
 }
 
 object NanoPostDestinationArgs {
-    const val ProfileId = "profileId"
-    const val ImageId = "imageId"
-    const val PostId = "postId"
+    const val IdArg = "id"
 }
 
 object NanoPostDestinations {
-    const val AuthRoute = NanoPostScreens.Auth
-    const val FeedRoute = NanoPostScreens.Feed
-    const val SearchRoute = NanoPostScreens.Search
-    const val ProfileRoute = NanoPostScreens.Profile
-    const val CreatePostRoute = NanoPostScreens.CreatePost
-    const val CreateProfileRoute = NanoPostScreens.CreateProfile
-    const val PostRoute = NanoPostScreens.Post
-    const val ImageRoute = NanoPostScreens.Image
+    const val RootRoute = RootScreen
+    const val AuthRoute = AuthScreen
+    const val LogoutRoute = LogoutScreen
+    const val FeedRoute = FeedScreen
+    const val ProfileRoute = "$ProfileScreen?$IdArg=$IdArg"
+    const val SubscribersRoute = "$SubscribersScreen?$IdArg=$IdArg"
+    const val ImagesRoute = "$ImagesScreen?$IdArg=$IdArg"
+    const val PostsRoute = "$PostsScreen?$IdArg=$IdArg"
+    const val ImageRoute = "$ImageScreen/$IdArg"
+    const val PostRoute = "$PostScreen/$IdArg"
+    const val CreatePostRoute = CreatePostScreen
+    const val CreateProfileRoute = CreateProfileScreen
+}
+
+@Stable
+class NanoPostNavigationActions(private val navController: NavHostController) {
+
+    fun navigateUp() {
+        navController.navigateUp()
+    }
+
+    fun switchToAuth() = navController.navigate(NanoPostDestinations.AuthRoute) {
+        popUpTo(navController.graph.id)
+    }
+
+    fun switchToFeed() = navController.navigate(NanoPostDestinations.FeedRoute) {
+        popUpTo(navController.graph.id)
+    }
+
+    fun openLogoutDialog() = navController.navigate(NanoPostDestinations.LogoutRoute)
+
+    fun navigateToProfile(id: String? = null) = navController.navigate(
+        ProfileScreen.let {
+            if (id != null) "$it?$IdArg=$id" else it
+        }
+    )
+
+    fun navigateToSubscribers(id: String? = null) = navController.navigate(
+        SubscribersScreen.let {
+            if (id != null) "$it?$IdArg=$id" else it
+        }
+    )
+
+    fun navigateToImages(id: String? = null) = navController.navigate(
+        ImagesScreen.let {
+            if (id != null) "$it?$IdArg=$id" else it
+        }
+    )
+
+    fun navigateToPosts(id: String? = null) = navController.navigate(
+        PostsScreen.let {
+            if (id != null) "$it?$IdArg=$id" else it
+        }
+    )
+
+    fun navigateToImage(id: String) = navController.navigate("$ImageScreen?$IdArg=$id")
+
+    fun navigateToPost(id: String) = navController.navigate("$PostScreen?$IdArg=$id")
+
+    fun navigateToCreatePost() = navController.navigate(NanoPostDestinations.CreatePostRoute)
+
+    fun navigateToCreateProfile() = navController.navigate(NanoPostDestinations.CreateProfileRoute)
 }
